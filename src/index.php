fe46b4fefc228909a56c434e3a8a19af42383bc8
Marina Delatorre <marina_mzd@yahoo.com.br>
2023-02-11 15:52:52 +0100

    KTB-9: Realizar validacao basica de front end

--- src/index.php
+++ src/index.php
@@ -9,6 +9,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0">
         <link href="css/estilo.css" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
         <script src="js/core.js" type="text/javascript"></script>
 
     </head>
@@ -24,7 +25,7 @@
                 <div class="row">
                     <label for="senha"> Senha </label>
                     <input type="password" name="senha" id="senha">
-                    <p class="error hidden">"Usuário e/ou senha inválidos!"</p>
+                    <p class="error hidden" id="erroLogin">"Usuário e/ou senha inválidos!"</p>
                 </div>
 
                 <div class="row">
--- src/js/core.js
+++ src/js/core.js
@@ -1,18 +1,27 @@
-// alternative to DOMContentLoaded
-document.onreadystatechange = function () {
-    if (document.readyState == "interactive") {
-        const btnEntrar = document.getElementById('entrar');
+$(document).ready(function(){
+    $("#entrar").click(function(event){
+        event.preventDefault();
+        console.log(event);
+        login(event.currentTarget.form[0].value, event.currentTarget.form[1].value);
+    });
+})
 
-
-        btnEntrar.onclick = basicValidation();
-
-        function basicValidation() {
-            alert('here we go');
-        }
-    
-    }
-
-
-   
-
+function login(user, pass){
+    if (validaLogin(user, pass)) {
+        console.log("vamos tentar logar");
+    };
 }
+
+function validaLogin(user, pass){
+    $valido = false;
+    if (user == "" || pass == "") {
+        console.log("um deles e vazio");
+        $("#erroLogin").removeClass("hidden");
+    } else {
+        console.log("segue o jogo");
+        $("#erroLogin").addClass("hidden");
+        $valido = true;
+    }
+    return $valido;
+}
+
